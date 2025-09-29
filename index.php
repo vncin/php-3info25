4d0c9436eb9a5b6191edbebafe3116642bc46c09
vncin <dantas.vinicius.tec@gmail.com>
2025-09-29 07:39:58 -0300

    PHP e PDO 26-09-2025

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro PHP</title>
+</head>
+<body>  
+    <form action="calcula.php" method="POST">
+        <label for="numero">Numero: </label>
+        <input type="text" name="numero" id="numero" required>
+
+        <label for="numero2">Numero 2: </label>
+        <input type="text" name="numero2" id="numero2" required>
+        
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
+
