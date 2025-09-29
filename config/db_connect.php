3a0c9416eb099aefb58986ba0e5304c94f165d47
vncin <dantas.vinicius.tec@gmail.com>
2025-09-29 08:01:37 -0300

    Aula 1 - 29-09-2025

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,7 +1,12 @@
 <?php
-
 $usuario_bd = 'root';
 $senha_bd = '123456';
 $nome_db = 'colegio';
-
-$pdo = new PDO("mysql: host=127.0.0.1; port=3306; dbname=$nome_db", "$usuario_bd", "$senha_bd");
+try {
+    $pdo = new PDO("mysql: host=127.0.0.1; port=3306; dbname=$nome_db", "$usuario_bd", "$senha_bd");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    echo "<h1>Conexão Estabelecida.</h1>";
+} catch (PDOException $e) {
+    echo "<h1 style='color: red;'>Error: </h1>" . $e->getMessage();
+}
