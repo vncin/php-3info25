4e177e2dc56b022758d657cfe50d61cf4e53d85e
vncin <dantas.vinicius.tec@gmail.com>
2025-10-08 08:48:24 -0300

    Aula 2 08-10-2025

---
+++ views/view_lista_alunos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_lista_alunos.css">
+    <title>Lista de Alunos</title>
+</head>
+<body>
+    <section class="principal">
+        <table class="principal-tabela">
+            <thead>
+                <th>ID Aluno</th>
+                <th>Nome </th>
+                <th>Data de Nascimento</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </section>
+</body>
+</html>
